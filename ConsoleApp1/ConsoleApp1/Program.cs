d66cfb00b0160ab4e37bd4b96d4bed7d020836dd
Adrian.Villorejo <adrian.villorejo@iesjulianmarias.es>
2019-01-30 10:21:08 +0100

    Main ha sido modificado para el ejemplo

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo 2");
+            Console.WriteLine("Modificar main");
         }
     }
 }
